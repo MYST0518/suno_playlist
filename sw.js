b2e1646881291d1e8c0a6c4e43e5fe4c1d4a901b
MYST0518 <108128217+MYST0518@users.noreply.github.com>
2026-02-01 20:25:37 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for SUNO Playlist Player PWA
-const CACHE_NAME = 'suno-playlist-v4';
+const CACHE_NAME = 'suno-playlist-v5';
 const OFFLINE_URL = '/';
 
 // Files to cache on install
